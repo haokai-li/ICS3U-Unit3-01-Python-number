701c8e32da72592748231e1c6787549d43a51876
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-16 16:10:58 +0000

    number 3

--- number.py
+++ number.py
@@ -17,13 +17,11 @@ def main():
 
     # output
     print("")
-
     print(
         "The answer: {0} + {1} = {2}".format(
             first_number, second_number, answer
         )
     )
-
     print("\nDone")
 
 
